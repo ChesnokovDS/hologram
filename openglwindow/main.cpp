2ffb97b296b1c20f0b2f5fbf1f4d3dbac4315127
Danila Chesnokov <ChesnokovDS@gmail.com>
2016-05-28 14:41:22 +0600

    убрал мусор из примера

--- openglwindow/main.cpp
+++ openglwindow/main.cpp
@@ -1,43 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the documentation of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include "openglwindow.h"
 
 #include <QtGui/QGuiApplication>
@@ -47,10 +7,10 @@
 
 #include <QtCore/qmath.h>
 
-class TriangleWindow : public OpenGLWindow
+class HalogramWindow : public OpenGLWindow
 {
 public:
-    TriangleWindow();
+    HalogramWindow();
 
     void initialize() Q_DECL_OVERRIDE;
     void render() Q_DECL_OVERRIDE;
@@ -58,13 +18,12 @@ public:
 private:
     GLuint m_posAttr;
     GLuint m_colAttr;
-    GLuint m_matrixUniform;
 
     QOpenGLShaderProgram *m_program;
     int m_frame;
 };
 
-TriangleWindow::TriangleWindow()
+HalogramWindow::HalogramWindow()
     : m_program(0)
     , m_frame(0)
 {}
@@ -76,7 +35,7 @@ int main(int argc, char **argv)
     QSurfaceFormat format;
     format.setSamples(16);
 
-    TriangleWindow window;
+    HalogramWindow window;
     window.setFormat(format);
     window.resize(640, 480);
     window.show();
@@ -88,21 +47,19 @@ int main(int argc, char **argv)
 
 static const char *vertexShaderSource =
     "\n attribute highp vec4 posAttr;"
-    "\n attribute lowp vec4 colAttr;"
-    "\n varying lowp vec4 col;"
-    "\n uniform highp mat4 matrix;"
+    "\n varying lowp vec2 TexCoord;"
     "\n void main() {"
-    "\n    col = colAttr;"
-    "\n    gl_Position = matrix * posAttr;"
+    "\n     gl_Position = posAttr;"
+    "\n     TexCoord = posAttr.xy;"
     "\n }";
 
 static const char *fragmentShaderSource =
-    "\n varying lowp vec4 col;"
+    "\n varying lowp vec2 TexCoord;"
     "\n void main() {"
-    "\n    gl_FragColor = col;"
+    "\n    gl_FragColor = vec4((vec2(1.0) + TexCoord) * 0.5, 0.0, 1.0);"
     "\n }";
 
-void TriangleWindow::initialize()
+void HalogramWindow::initialize()
 {
     m_program = new QOpenGLShaderProgram(this);
     m_program->addShaderFromSourceCode(QOpenGLShader::Vertex  , vertexShaderSource  );
@@ -110,10 +67,9 @@ void TriangleWindow::initialize()
     m_program->link();
     m_posAttr = m_program->attributeLocation("posAttr");
     m_colAttr = m_program->attributeLocation("colAttr");
-    m_matrixUniform = m_program->uniformLocation("matrix");
 }
 
-void TriangleWindow::render()
+void HalogramWindow::render()
 {
     const qreal retinaScale = devicePixelRatio();
     glViewport(0, 0, width() * retinaScale, height() * retinaScale);
@@ -122,32 +78,21 @@ void TriangleWindow::render()
 
     m_program->bind();
 
-    QMatrix4x4 matrix;
-    matrix.perspective(60.0f, 4.0f/3.0f, 0.1f, 100.0f);
-    matrix.translate(0, 0, -2);
-    matrix.rotate(100.0f * m_frame / screen()->refreshRate(), 0, 1, 0);
-
-    m_program->setUniformValue(m_matrixUniform, matrix);
-
     GLfloat vertices[] = {
-        0.0f, 0.707f,
-        -0.5f, -0.5f,
-        0.5f, -0.5f
-    };
-
-    GLfloat colors[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f
+        -1.0f,  1.0f,
+         1.0f,  1.0f,
+         1.0f, -1.0f,
+         1.0f, -1.0f,
+        -1.0f, -1.0f,
+        -1.0f,  1.0f
     };
 
     glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
-    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
 
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
